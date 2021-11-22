edc000cfd0f3993a685e6abe6287a0e2298ce41d
HoangTrung <ronaldisbestt@gmail.com>
2021-11-22 21:11:39 +0700

    add console for index

--- dog.js
+++ dog.js
@@ -1,4 +1,11 @@
+var chalk = require('chalk');
+
 function Dog(name) {
 	this.name = name;
 	this.dead = false;
 }
+Dog.prototype.sayHi = function() {
+	console.log('Hi! I am a dog. My name is ' + chalk.red(this.name));
+};
+
+module.exports = Dog;
---
+++ index.js
@@ -0,0 +1,6 @@
+var Dog = require('./Dog');
+
+var dog = new Dog('KKK');
+dog.sayHi();
+
+console.log('Bye Bye');
